028489d02d4390450a8fdbab52578d36b4c80246
sejum-x <franchukivan123@gmail.com>
2024-02-25 20:53:56 +0200

    Final lab1

--- script.js
+++ script.js
@@ -4,40 +4,62 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function openTab(evt, tabName) {
-    // Declare all variables
     var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    // Show the current tab, and add an "active" class to the link that opened the tab
     document.getElementById(tabName).style.display = "grid";
     evt.currentTarget.className += " active";
 }
 
 
 var studentsData = [
-    { group: "A", name: "Sofiyka Yaroshovych", gender: "Female", birthday: "2000-01-01", status: "Active" },
-    { group: "B", name: "Nazik Nafta", gender: "Male", birthday: "2000-02-02", status: "Inactive" },
-    { group: "B", name: "Fitia Boem", gender: "Male", birthday: "2000-02-02", status: "Active" },
-    { group: "B", name: "Yurchyk Starosta", gender: "Male", birthday: "2000-02-02", status: "Inactive" },
-    { group: "B", name: "Pes Patron", gender: "Male", birthday: "2000-02-02", status: "Inactive" },
-    { group: "B", name: "Max sah sakh sahk", gender: "Male", birthday: "2000-02-02", status: "Active" },
-    { group: "B", name: "Asiiiiykka", gender: "Male", birthday: "2000-02-02", status: "Inactive" },
-    // Add more data here
+    { group: "PZ-28", name: "Sofiyka Yaroshovych", gender: "Female", birthday: "2000-01-01", status: "Active" },
+    { group: "PZ-28", name: "Nazik Nafta", gender: "Male", birthday: "2000-02-02", status: "Inactive" },
+    { group: "PZ-28", name: "Fitia Boem", gender: "Male", birthday: "2000-02-02", status: "Active" },
+    { group: "PZ-28", name: "Yurchyk Starosta", gender: "Male", birthday: "2000-02-02", status: "Inactive" },
+    { group: "PZ-28", name: "Pes Patron", gender: "Male", birthday: "2000-02-02", status: "Inactive" },
+    { group: "PZ-28", name: "Max sah sakh sahk", gender: "Male", birthday: "2000-02-02", status: "Active" },
+    { group: "PZ-28", name: "Asiiiiykka", gender: "Male", birthday: "2000-02-02", status: "Inactive" },
 ];
 
 var currentPage = 1;
 var studentsPerPage = 10;
+
+function createStudent() {
+    var group = document.getElementById('group').value;
+    var fname = document.getElementById('fname').value;
+    var lname = document.getElementById('lname').value;
+    var gender = document.getElementById('gender').value;
+    var bday = document.getElementById('bday').value;
+
+    if (!group || !fname || !lname || !gender || !bday) {
+        alert('Please fill in all fields.');
+        return;
+    }
+
+    addStudent(group, fname + ' ' + lname, gender, bday);
+}
+
+function addStudent(group, name, gender, birthday) {
+    var newStudent = {
+        group: group,
+        name: name,
+        gender: gender,
+        birthday: birthday,
+        status: "Active"
+    };
+    studentsData.push(newStudent);
+    renderStudents(currentPage);
+}
 
 function renderStudents(page) {
     var startIndex = (page - 1) * studentsPerPage;
@@ -63,12 +85,14 @@ function renderStudents(page) {
 
         var optionsCell = row.insertCell(6);
         var deleteButton = document.createElement("button");
-        deleteButton.innerHTML = '<img src="assets/delete_icon.svg" alt="Delete Icon" style="width: 16px; height: 16px;">';
+        deleteButton.innerHTML = '<img src="assets/delete_icon_white.svg" alt="Delete Icon" style="width: 16px; height: 16px;">';
+        deleteButton.className = "delete-button";
         deleteButton.onclick = function() {
             deleteStudent(index + startIndex);
         };
         var editButton = document.createElement("button");
-        editButton.innerHTML = '<img src="assets/edit_icon.svg" alt="Edit Icon" style="width: 16px; height: 16px;">';
+        editButton.innerHTML = '<img src="assets/edit_icon_white.svg" alt="Edit Icon" style="width: 16px; height: 16px;">';
+        editButton.className = "edit-button";
         editButton.onclick = function() {
             editStudent(index + startIndex);
         };
@@ -79,37 +103,59 @@ function renderStudents(page) {
         optionsCell.appendChild(spacer);
         optionsCell.appendChild(editButton);
     });
-
-
-
+    updateAllCheckboxes(selectAllCheckbox.checked);
 }
 
-function createStudent() {
-    var group = document.getElementById('group').value;
-    var fname = document.getElementById('fname').value;
-    var lname = document.getElementById('lname').value;
-    var gender = document.getElementById('gender').value;
-    var bday = document.getElementById('bday').value;
 
-    addStudent(group, fname + ' ' + lname, gender, bday);
+
+var modal = document.getElementById("addModal");
+var closeButton = modal.querySelector(".close");
+function OpenAddStudentModal() {
+    modal.style.display = "block";
+}
+function CloseAddStudentModal() {
+    modal.style.display = "none";
+}
+closeButton.addEventListener("click", CloseAddStudentModal);
+
+window.addEventListener("click", function(event) {
+    if (event.target === modal) {
+        CloseAddStudentModal();
+    }
+});
+
+var selectAllCheckbox = document.querySelector("#studentsTable thead input[type='checkbox']");
+selectAllCheckbox.addEventListener('change', function() {
+    updateAllCheckboxes(selectAllCheckbox.checked);
+});
+function updateAllCheckboxes(state) {
+    var checkboxes = document.querySelectorAll("#studentsTable tbody input[type='checkbox']");
+    checkboxes.forEach(function(checkbox) {
+        checkbox.checked = state;
+    });
 }
 
-function addStudent(group, name, gender, birthday) {
-    var newStudent = {
-        group: group,
-        name: name,
-        gender: gender,
-        birthday: birthday,
-        status: "Active"
-    };
-    studentsData.push(newStudent);
-    renderStudents(currentPage);
-}
 
+
+var currentDeleteIndex;
 function deleteStudent(index) {
-    studentsData.splice(index, 1);
-    renderStudents(currentPage);
+    currentDeleteIndex = index;
+    OpenDeleteConfirmationModal();
 }
+function confirmDeleteStudent() {
+    studentsData.splice(currentDeleteIndex, 1);
+    renderStudents(currentPage);
+    CloseDeleteConfirmationModal();
+}
+function OpenDeleteConfirmationModal() {
+    var deleteModal = document.getElementById("deleteModal");
+    deleteModal.style.display = "block";
+}
+function CloseDeleteConfirmationModal() {
+    var deleteModal = document.getElementById("deleteModal");
+    deleteModal.style.display = "none";
+}
+
 
 function editStudent(index) {
     currentEditIndex = index;
@@ -123,8 +169,6 @@ function editStudent(index) {
 
     OpenEditStudentModal();
 }
-
-
 function OpenEditStudentModal() {
     var editModal = document.getElementById("editModal");
     editModal.style.display = "block";
@@ -135,6 +179,16 @@ function CloseEditStudentModal() {
     editModal.style.display = "none";
 }
 
+var EditModal = document.getElementById("editModal");
+var closeButton = EditModal.querySelector(".close");
+closeButton.addEventListener("click", CloseEditStudentModal);
+
+window.addEventListener("click", function(event) {
+    if (event.target === editModal) {
+        CloseEditStudentModal();
+    }
+});
+
 function updateStudent() {
     var group = document.getElementById('edit-group').value;
     var fname = document.getElementById('edit-fname').value;
@@ -142,25 +196,23 @@ function updateStudent() {
     var gender = document.getElementById('edit-gender').value;
     var bday = document.getElementById('edit-bday').value;
 
+    if (!group || !fname || !lname || !gender || !bday) {
+        alert('Please fill in all fields.');
+        return;
+    }
+
     var editedStudent = {
         group: group,
         name: fname + ' ' + lname,
         gender: gender,
         birthday: bday,
-        status: "Active" // Assuming status is not editable in the modal
+        status: "Active" // not editable
     };
 
-    studentsData[currentEditIndex] = editedStudent; // Assuming currentEditIndex is the index of the student being edited
+    studentsData[currentEditIndex] = editedStudent;
     renderStudents(currentPage);
     CloseEditStudentModal();
 }
-
-
-
-// function editStudent(index) {
-//     console.log("Editing student at index:", index);
-// }
-
 
 
 function previousPage() {
@@ -177,3 +229,31 @@ function nextPage() {
         renderStudents(currentPage);
     }
 }
+
+
+window.addEventListener('resize', function() {
+    if (window.innerWidth < 600) {
+        document.querySelector('#studentsTable th:nth-child(6)').textContent = 'S';
+        document.querySelector('#studentsTable th:nth-child(2)').textContent = 'G';
+        document.querySelector('#studentsTable th:nth-child(4)').textContent = 'G';
+        document.querySelectorAll('#studentsTable td:nth-child(4)').forEach(function(cell) {
+            if (cell.textContent.trim() === 'Male') {
+                cell.textContent = 'M';
+            } else if (cell.textContent.trim() === 'Female') {
+                cell.textContent = 'F';
+            }
+        });
+    } else {
+        document.querySelector('#studentsTable th:nth-child(6)').textContent = 'Status';
+        document.querySelector('#studentsTable th:nth-child(4)').textContent = 'Gender';
+        document.querySelector('#studentsTable th:nth-child(2)').textContent = 'Group';
+        document.querySelectorAll('#studentsTable td:nth-child(4)').forEach(function(cell) {
+            if (cell.textContent.trim() === 'M') {
+                cell.textContent = 'Male';
+            } else if (cell.textContent.trim() === 'F') {
+                cell.textContent = 'Female';
+            }
+        });
+    }
+});
+
